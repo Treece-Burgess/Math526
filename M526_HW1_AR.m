dcec16e5392db59e11c18ab71ced9d03725d9a0b
babar-yoiny <61997987+babar-yoiny@users.noreply.github.com>
2023-02-28 15:38:34 -0500

    Update M526_HW1_AR.m

--- M526_HW1_AR.m
+++ M526_HW1_AR.m
@@ -2,36 +2,36 @@ clear all;
 
 load('streambed_data.mat')
 
-u_func=@(z)(abs(z)<40).*(-15+0.01.*z.^2)+(abs(z)>=40).*(z.*0);%(abs(z)<40).*(-15*exp(-(1/900)*z.^2))+(abs(z)>=40).*(z.*0); %mean(y)*z.^0;% %(abs(z)<40).*(-17*cos((z.*pi)./80))+(abs(z)>=40).*(0); 
+u_func=@(z)(abs(z)<40).*(-15+0.01.*z.^2)+(abs(z)>=40).*(z.*0);%(abs(z)<40).*(-15*exp(-(1/1000)*z.^2))+(abs(z)>=40).*(z.*0); %mean(y)*z.^0;% %(abs(z)<40).*(-17*cos((z.*pi)./80))+(abs(z)>=40).*(0); 
 lambda=1;
 l=5;
 n=101;
 s=10000;                         
 t_p=linspace(-100,100,n);
 
-C_tp=lambda^2*exp(-(t_p'-t_p).^2/(2*l^2));
-C_d=lambda^2*exp(-(x'-x).^2/(2*l^2));
+C_tptp=lambda^2*exp(-(t_p'-t_p).^2/(2*l^2));
+C_dd=lambda^2*exp(-(x'-x).^2/(2*l^2));
 C_dtp=lambda^2*exp(-(x'-t_p).^2/(2*l^2));
 C_tpd=lambda^2*exp(-(t_p'-x).^2/(2*l^2));
 
-L_pr=chol(C_tp);
+L_pr=chol(C_tptp);
 u_pr=u_func(t_p);
 
 prior=ones(s,1)*u_func(t_p)+normrnd(0,1,s,n)*L_pr;
 
-% figure()
-% hold on
-% plot(t_p,prior, 'Color', "#888c89",'LineWidth',0.5)
-% plot(t_p,mean(prior), 'Color','m','LineWidth',0.5)
-% errorbar(x,y,d,'o','Color','black','LineWidth',1.5)
-% title('Input Data vs Prior')
-% ylabel('Output (y)')
-% xlabel('Input (x)')
-% hold off
+figure()
+hold on
+plot(t_p,prior, 'Color', "#888c89",'LineWidth',0.5)
+plot(t_p,mean(prior), 'Color','m','LineWidth',0.5)
+errorbar(x,y,d,'o','Color','black','LineWidth',1.5)
+title('Input Data vs Prior')
+ylabel('Output (y)')
+xlabel('Input (x)')
+hold off
 
-G=inv(C_d+diag(d.^2));
+G=inv(C_dd+diag(d.^2));
 U_pt=u_func(t_p)+(y-u_func(x))*G'*C_dtp;
-C_pt=C_tp-C_tpd*G*C_dtp;
+C_pt=C_tptp-C_tpd*G*C_dtp;
 L_pt=chol(C_pt);
 post=ones(s,1)*U_pt+normrnd(0,1,s,n)*L_pt;
 
@@ -88,15 +88,15 @@ for i=1:length(alpha)
 I_4(i)=(1/s)*sum(mc_4); 
 end
 
-
-cdf_5=mvncdf([-10,-10,-10,-10,-10], u_alpha, sigma_alpha);
+cdf_5=mvncdf([-20,-20,-20,-20,-20], u_alpha, sigma_alpha);
 for i=1:s
     mc_5=zeros(s,1);
     
     if (post(i,a) > -10) && (post(i,b) > -10) && (post(i,c) > -10)...
-            && post(i,d) > -10 && post(i,e) > -10
+            && (post(i,d) > -10) && (post(i,e) > -10)
         mc_5(i)=1;
     else
     end  
 end
 I_5=(1/s)*sum(mc_5);
+
